6bad5b860f763ecf2792906aa6378617f43920ff
AnngHack <116359244+AnngHack@users.noreply.github.com>
2023-07-30 21:59:15 +0530

    Update slideshare.py

--- slideshare.py
+++ slideshare.py
@@ -1,24 +1,57 @@
+import requests
 import os
 from PIL import Image
-import requests
 from pypdf import PdfMerger
+
+def get_first_line(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        lines = response.text.split('\n')
+        first_line = lines[0]
+        return first_line
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the website: {e}")
+        return None
+
+
+website_url = input('Enter the slideshare url: ')
+
+first_line_variable = get_first_line(website_url)
+
+link_list = []
+if first_line_variable is not None:
+    break_line = first_line_variable.split()
+    for link in break_line:
+        if link.startswith("https://image.slidesharecdn.com/"):
+            jpg_link = link.split('?')[0]
+            if '2048.jpg' in jpg_link:
+                link_list.append(jpg_link)
+
+else:
+    print("Failed to fetch the website.")
+
+final_list = []
+for item in link_list:
+    if item not in final_list:
+        final_list.append(item)
+end = len(final_list)
+
 i = 1
-end = 51 #CHANGE THIS TO THE MAXIMUM NUMBER OS SLIDE
 while i <= end:
-    url = f"https://image.slidesharecdn.com/datasciencecorner-221130165458-60b0d249/75/introduction-to-data-science-{i}-2048.jpg"
-    #..../75/...{i}-2048.jpg
-    response = requests.get(url)
-    open(f"{i}.jpg", "wb").write(response.content)
-    output_dir = '../slideshare'
-    source_dir = '../slideshare'
-    for file in os.listdir(source_dir):
-        if file.split('.')[-1] in ('png', 'jpg', 'jpeg'):
-            image = Image.open(os.path.join(source_dir, file))
-            image_converted = image.convert('RGB')
-            image_converted.save(os.path.join(output_dir, '{0}.pdf'.format(file.split('.')[-2])))
-    os.remove(f"{i}.jpg")
-    print(f"{i} slide downloaded...")
-    i += 1
+    for url in final_list:
+        response = requests.get(url)
+        open(f"{i}.jpg", "wb").write(response.content)
+        output_dir = '../slideshare'
+        source_dir = '../slideshare'
+        for file in os.listdir(source_dir):
+            if file.split('.')[-1] in ('png', 'jpg', 'jpeg'):
+                image = Image.open(os.path.join(source_dir, file))
+                image_converted = image.convert('RGB')
+                image_converted.save(os.path.join(output_dir, '{0}.pdf'.format(file.split('.')[-2])))
+        os.remove(f"{i}.jpg")
+        print(f"{i} slide downloaded...")
+        i += 1
 j = 1
 pdf_list = []
 while j <= end:
@@ -27,7 +60,7 @@ while j <= end:
 merger = PdfMerger()
 for pdf in pdf_list:
     merger.append(pdf)
-merger.write("result.pdf")
+merger.write("Output.pdf")
 merger.close()
 k = 1
 pdf_list = []
